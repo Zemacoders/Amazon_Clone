c8b703152281e7764a756e159659e047cdb3f5cf
teklu abayneh <tekluabayneh@gmail.com>
2025-03-20 16:09:18 +0300

    Fix syntax in App.jsx by removing extra return and maintain layout with Routers

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <>
       <h1 className="text-center  text-4xl text-red-500">Amazon Clone</h1>
+      <Routers />
     </>
   );
-  return <Routers/>
+  return;
 }
 export default App;
