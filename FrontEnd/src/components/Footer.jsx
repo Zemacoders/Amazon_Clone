58210b52e4486afe682818320b29cf79e223c783
devhan-hub <hanabduman@gmail.com>
2025-03-22 14:33:47 +0300

    feat: add footer component

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -1,109 +1,163 @@
 
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
+
+  const year = new Date().getFullYear();
   return (
-    <div>
-      Footer
-    </div>
-  )
+    <footer className='pt-[60px] pb-[30px] bg-secondary '>
+
+     <div className='sm:px-4 px-12 xl:w-[90%] sm:w-[98%] w-full mx-auto'>
+
+      <div className=" mx-auto grid grid-cols-12 justify-between gap-4   px-2">      
+      <div className='col-span-12 md:col-span-3 sm:col-span-6 text-white'>
+      <div className="footer_quick-link">
+        <h4 className="mb-8 text-white text-lg capitalize">get to know Us</h4>
+        <ul className='mb-3 bg-transparent text-white space-y-1'>
+        <li className='px-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> Careers
+          </Link>
+        </li>
+        <li className='px-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Blog
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> About Us
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Invester Relations
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> Amazon Devices
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Amazon Science
+          </Link>
+        </li>
+        </ul>
+      </div>
+      </div>
+
+      <div className='col-span-12 md:col-span-3 sm:col-span-6 text-white'>
+      <div className="footer_quick-link">
+        <h4 className="mb-8 text-white text-lg capitalize">Make money with us</h4>
+        <ul className='mb-3 bg-transparent text-white space-y-1'>
+        <li className='px-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> Sell Products
+          </Link>
+        </li>
+        <li className='px-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Amazon Business
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> Sell Apps
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Become Affilate
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> Advertse Products
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Self  Punblish with us
+          </Link>
+        </li>
+        </ul>
+      </div>
+      </div>
+
+      <div className=' md:col-span-3 sm:col-span-6 col-span-12'>
+      <div className="footer_quick-link">
+      <h4 className="mb-8 text-white text-lg capitalize">Amazon Payment Product</h4>
+        <ul className='mb-3 space-y-1' >
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> Amazon Business Card
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Shop with points
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Reload your balance
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Amazon Currency converter
+          </Link>
+        </li>
+        </ul>
+      </div>
+      </div>
+
+      <div className=' md:col-span-3 sm:col-span-6 col-span-12'>
+      <div className="footer_quick-link">
+      <h4 className="mb-8 text-white text-lg capitalize">Lets us help you</h4>
+        <ul className='mb-3 space-y-1'>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> your account
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Your orders
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Shipping Rates & Policies
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Return & Replacement
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Maage your content
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>help center
+          </Link>
+        </li>
+        </ul>
+      </div>
+      </div>
+
+      <div className='col-span-12  gap-12 items-center border-t-2 border-white pt-8  mt-9'>
+      <ul className=' bg-transparent text-white  flex flex-wrap items-center gap-2 sm:mb-2 mb-6 justify-between max-w-[700px] mx-auto'>
+        <li className='px-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'> Conditions of use
+          </Link>
+        </li>
+        <li className='px-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>privacy notice
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Customer health data privacy
+          </Link>
+        </li>
+        <li className='ps-0 border-0'>
+          <Link className='text-white opacity-75 hover:opacity-100 capitalize ' to='#'>Your ads perivacy choices
+          </Link>
+        </li>
+        
+        </ul>
+      <p className="  text-white opacity-90 mx-auto text-lg text-center">
+        &copy;  1996-{year}, Amazon.com , inc , or its affiliates
+      </p>
+      </div>
+      </div>
+      </div>
+    </footer>
+    )
 }
 
 export default Footer
-
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// import { Container , Row , Col, ListGroup, ListGroupItem } from 'reactstrap'
-// const Footer = () => {
-
-//   const year = new Date().getFullYear();
-//   return (
-//     <footer className='footer'>
-//      <Container>
-//       <Row>
-//         <Col lg='4' md='6'>
-//         <div className="logo">
-//             <div className='logo_description'>
-//               <h1 className='text-white'>Multimart</h1>
-//             </div>
-//             </div>
-//             <p className="footer_text mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cum modi molestiae cumque fugit sed omnis placeat optio non exercitationem?</p>
-//         </Col>
-//         <Col lg='3' md='3'>
-//         <div className="footer_quick-link">
-//           <h4 className="quick_link-title mb-2">Top Catagory</h4>
-//           <ListGroup className='mb-3'>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='#'> Mobile Phones
-//               </Link>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='#'>Modern Sofa
-//               </Link>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='#'> Arm Chair
-//               </Link>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='#'>Smart Watches
-//               </Link>
-//             </ListGroupItem>
-//           </ListGroup>
-//         </div>
-//         </Col>
-//         <Col lg='2' md='3'>
-//         <div className="footer_quick-link">
-//           <h4 className="quick_link-title mb-2">Usefull Links</h4>
-//           <ListGroup className='mb-3'>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='/shop'> Shop
-//               </Link>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='/cart'>Cart
-//               </Link>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='/login'> Login
-//               </Link>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//               <Link to='/'>privacy Policy
-//               </Link>
-//             </ListGroupItem>
-//           </ListGroup>
-//         </div>
-//         </Col>
-//         <Col lg='3' md='2'>
-//         <div className="footer_quick-link ">
-//           <h4 className="quick_link-title mb-2">Contact</h4>
-//           <ListGroup className='mb-3 footer-contact'>
-//             <ListGroupItem className='ps-0 border-0 d-flex align-center gap-2'>
-//               <span> <i class ="ri-map-pin-line"></i></span>
-//               <p>123 ZindaBazaar , Sylhet , Bangladish</p>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//             <span> <i class ="ri-phone-line"></i></span>
-//               <p>+2519512345</p>
-//             </ListGroupItem>
-//             <ListGroupItem className='ps-0 border-0'>
-//             <span> <i class ="ri-mail-line"></i></span>
-//             <p>example@gmail.com</p>
-//             </ListGroupItem>
-           
-//           </ListGroup>
-//         </div>
-//         </Col>
-//         <Col lg='12'>
-//         <p className="footer_copyright">
-//           Copyright {year} developed by Hanan Abdulshikur. All rights reserved.
-//         </p>
-//         </Col>
-//       </Row>
-//      </Container>
-//     </footer>
-//   )
-// }
-
-// export default Footer
