67c89a8f3ee2efbf90be3abae905f1bd9dfd9cb7
devhan-hub <hanabduman@gmail.com>
2025-03-24 11:17:00 +0300

    feat: add images for hero and category sections

---
+++ FrontEnd/src/assets/heroSlideData.js
@@ -0,0 +1,46 @@
+import Electronics from './electronics.jpg'
+import Fashion from './fashion2.jpg'
+import Home from './home.jpg'
+import Beauty from './beauty.jpg'
+import Sports from './sports.jpg'
+
+export const slider = [
+   
+    {
+        category: "Sports & Outdoors",
+        subheading: "Limited-Time Deal. Grab Now!",
+        mainHeading: "Powerful Workouts Await!",
+        button: "Shop Now",
+        image: Sports
+    },
+    {
+        category: "Electronics",
+        subheading: "Latest Tech Deals. Don't Miss Out!",
+        mainHeading: "Smart Gadgets, Smarter Living!",
+        button: "Shop Now",
+        image: Electronics
+    },
+    {
+        category: "Fashion",
+        subheading: "Stay Stylish All Season Long!",
+        mainHeading: "Trendy Looks Just for You!",
+        button: "Explore Fashion",
+        image: Fashion
+    },
+    {
+        category: "Home & Kitchen",
+        subheading: "Transform Your Space Today!",
+        mainHeading: "Make Home Life Effortless!",
+        button: "Upgrade Now",
+        image: Home
+    },
+    {
+        category: "Beauty & Personal Care",
+        subheading: "Glow Up Starts Here!",
+        mainHeading: "Feel Confident. Look Radiant!",
+        button: "Browse Beauty",
+        image: Beauty
+    },
+    
+
+];
